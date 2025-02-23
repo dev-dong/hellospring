0c7a3956125504078f464baab4b2c454b026b9ca
dong <dho0647@naver.com>
2025-02-23 18:07:51 +0900

    관심사 분리 - 상속을 통한 확장

---
+++ src/main/java/dongspring/hellospring/WebApiExRatePaymentService.java
@@ -0,0 +1,29 @@
+package dongspring.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService {
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("http://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // InputStream은 File이나 Network를 통해서 넘겨오는 데이터를 byte 형태로 return
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        return exRate;
+    }
+}
